716f3b55395bca0d2f17bf55000e1e8b33a5bf95
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-05-23 23:32:35 +0200

    Start: simple application with edit box as input, label as output

---
+++ README.md
@@ -0,0 +1,17 @@
+# Castle Game Engine - Querying OpenAI
+
+Use OpenAI API from CGE. Just a few tests.
+
+Using [Castle Game Engine](https://castle-engine.io/).
+
+## Building
+
+Compile by:
+
+- [CGE editor](https://castle-engine.io/editor). Just use menu items _"Compile"_ or _"Compile And Run"_.
+
+- Or use [CGE command-line build tool](https://castle-engine.io/build_tool). Run `castle-engine compile` in this directory.
+
+- Or use [Lazarus](https://www.lazarus-ide.org/). Open in Lazarus `castle_openai_standalone.lpi` file and compile / run from Lazarus. Make sure to first register [CGE Lazarus packages](https://castle-engine.io/lazarus).
+
+- Or use [Delphi](https://www.embarcadero.com/products/Delphi). Open in Delphi `castle_openai_standalone.dproj` file and compile / run from Delphi. See [CGE and Delphi](https://castle-engine.io/delphi) documentation for details.
---
+++ code/gameviewmain.pas
@@ -0,0 +1,65 @@
+{ Copyright 2024-2024 Michalis Kamburelis.
+  See LICENSE for licensing. }
+
+{ Main view, where most of the application logic takes place. }
+unit GameViewMain;
+
+interface
+
+uses Classes,
+  CastleVectors, CastleComponentSerialize,
+  CastleUIControls, CastleControls, CastleKeysMouse;
+
+type
+  { Main view, where most of the application logic takes place. }
+  TViewMain = class(TCastleView)
+  published
+    { Components designed using CGE editor.
+      These fields will be automatically initialized at Start. }
+    LabelFps: TCastleLabel;
+    ButtonSend: TCastleButton;
+    EditQuery: TCastleEdit;
+    LabelAnswer: TCastleLabel;
+  private
+    procedure ClickSend(Sender: TObject);
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure Start; override;
+    procedure Update(const SecondsPassed: Single; var HandleInput: Boolean); override;
+  end;
+
+var
+  ViewMain: TViewMain;
+
+implementation
+
+uses SysUtils;
+
+{ TViewMain ----------------------------------------------------------------- }
+
+constructor TViewMain.Create(AOwner: TComponent);
+begin
+  inherited;
+  DesignUrl := 'castle-data:/gameviewmain.castle-user-interface';
+end;
+
+procedure TViewMain.Start;
+begin
+  inherited;
+  ButtonSend.OnClick := {$ifdef FPC}@{$endif} ClickSend;
+end;
+
+procedure TViewMain.Update(const SecondsPassed: Single; var HandleInput: Boolean);
+begin
+  inherited;
+  { This virtual method is executed every frame (many times per second). }
+  Assert(LabelFps <> nil, 'If you remove LabelFps from the design, remember to remove also the assignment "LabelFps.Caption := ..." from code');
+  LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
+end;
+
+procedure TViewMain.ClickSend(Sender: TObject);
+begin
+  LabelAnswer.Caption := 'You asked: ' + EditQuery.Text;
+end;
+
+end.
